eb299da0b4ccdefcb19fcc749ee944ffc379d30d
Helvin <hjwc86@gmail.com>
2020-06-23 16:52:13 +0200

    BOUCLES DE BASE

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -4,7 +4,36 @@ public class DeclarationApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		byte b = 1;
+		short s = -200;
+		int i = 200;
+		long l = 10000L;
+		float f = 2.5f;
+		double d = 6.4D;
+		char c = 'c';
+		boolean bool = true;
+		String str = "hey";
+		
+		System.out.println(d);
+		
+		String randomString = "Voici le résultat d’un calcul : 1+5=6";
+		
+		System.out.println(randomString);
+		
+		String randomString2 = "Voici le résultat d’un calcul : \n 1+5=6";
+		
+		System.out.println(randomString2);
+		
+		System.out.println("Salut");
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(bool);
+		System.out.println(str);
+		
 	}
 
 }
