34a45dbee7430f2c65b987fd760ba724cb6211e6
Helvin <hjwc86@gmail.com>
2020-06-23 13:13:34 +0200

    Declaration

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -1,5 +1,13 @@
 package fr.declaration.variable;
 
 public class DeclarationApp {
-
+	byte b = 1;
+	short s = -200;
+	int i = 200;
+	long l = 10000L;
+	float f = 2.5f;
+	double d = 6.4D;
+	char c = 'c';
+	boolean bool = true;
+	String str = "hey";
 }
