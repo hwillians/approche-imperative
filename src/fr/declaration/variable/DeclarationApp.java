1df3de28552ccc1bfbced2086ec91bb9d3683a51
Helvin <hjwc86@gmail.com>
2020-06-23 13:17:12 +0200

    DeclarationCorrigé

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -1,13 +1,10 @@
 package fr.declaration.variable;
 
 public class DeclarationApp {
-	byte b = 1;
-	short s = -200;
-	int i = 200;
-	long l = 10000L;
-	float f = 2.5f;
-	double d = 6.4D;
-	char c = 'c';
-	boolean bool = true;
-	String str = "hey";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
