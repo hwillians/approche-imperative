d452a43fe126fd4834f6b96127425a3827c904e2
Helvin <hjwc86@gmail.com>
2020-06-23 17:36:50 +0200

    PARCOURS D’UN TABLEAU D’ENTIERS

---
+++ src/fr/boucles/ExerciceTableauEntier.java
@@ -0,0 +1,31 @@
+package fr.boucles;
+
+public class ExerciceTableauEntier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Déclarez un tableau d’entiers contenant tous les entiers de 1 à 10
+		
+		int [] entiers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		
+		// Affichez le premier élément du tableau
+		System.out.println(entiers[0]);
+		
+		System.out.println("\n --------- \n");
+		// Affichez la longueur du tableau en utilisant la propriété length
+		
+		System.out.println(entiers.length);
+		
+		System.out.println("\n --------- \n");
+		// Affichez le dernier élément du tableau en utilisant la propriété length
+		
+		System.out.println(entiers[entiers.length-1]);
+		
+		System.out.println("\n --------- \n");
+		// Modifiez la valeur de l’élément d’index 4 et donnez lui la valeur 8.
+		
+		entiers[4]=8;
+		System.out.println(entiers[4]);
+	}
+
+}
